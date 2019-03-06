0493a05cf667c8ed3e44b6d937e5d7bc1c00fe9d
Diana <dianafbernardo@gmail.com>
2019-03-06 15:26:45 +0100

    Created Game Logic class

---
+++ src/main/java/cow/fedex/GameLogic.java
@@ -0,0 +1,73 @@
+package cow.fedex;
+
+public class GameLogic {
+
+  private int level;
+  private int totalQuestions;
+  private int askedQuestions;
+  private int correctGuesses;
+
+  public GameLogic() {
+    this.level = 1;
+    this.askedQuestions = 0;
+    this.correctGuesses = 0;
+  }
+
+  public GameLogic(int totalQuestions) {
+    this.level = 1;
+    this.totalQuestions = totalQuestions;
+    this.askedQuestions = 0;
+    this.correctGuesses = 0;
+  }
+
+  public boolean score() {
+    double result = (double) (this.correctGuesses / this.askedQuestions);
+    if (result >= 0.8) {
+      return true;
+    }
+    return false;
+  }
+
+  public void increaseLevel(Integer level) {
+    if (level < 5) {
+      if (this.askedQuestions == this.totalQuestions) {
+        if (score()) {
+          level++;
+        }
+      }
+    }
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public int getTotalQuestions() {
+    return totalQuestions;
+  }
+
+  public void setTotalQuestions(int totalQuestions) {
+    this.totalQuestions = totalQuestions;
+  }
+
+  public int getAskedQuestions() {
+    return askedQuestions;
+  }
+
+  public void setAskedQuestions(int askedQuestions) {
+    this.askedQuestions = askedQuestions;
+  }
+
+  public int getCorrectGuesses() {
+    return correctGuesses;
+  }
+
+  public void setCorrectGuesses(int correctGuesses) {
+    this.correctGuesses = correctGuesses;
+  }
+}
+
