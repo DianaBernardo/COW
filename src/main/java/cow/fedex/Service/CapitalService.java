9991a47ac7e68783888e16257269b58f14094d36
Mark Palotas <ppmark@gmail.com>
2019-03-07 10:54:58 +0100

    add thymeleaf for capital questions

--- src/main/java/cow/fedex/Service/CapitalService.java
+++ src/main/java/cow/fedex/Service/CapitalService.java
@@ -5,6 +5,7 @@ import cow.fedex.Repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +19,6 @@ public class CapitalService {
     this.countryService = countryService;
   }
 
-
 //  public Map<Integer, String> countryOptions(int difficulty) {
 //    Map<Integer, Country> countryMap = countryService.options(difficulty);
 //    Map<Integer, String> countryOptionsMap = new HashMap<Integer, String>() {
@@ -31,16 +31,25 @@ public class CapitalService {
 //    return countryOptionsMap;
 //  }
 
-  public Map<String, String> countryOptions(int difficulty) {
-    Map<Integer, Country> countryMap = countryService.options(difficulty);
-    Map<String, String> countryOptionsMap = new HashMap<String, String>();
-        for (int i = 0; i < countryMap.size(); i++) {
-          countryOptionsMap.put(countryMap.get(i).getName(), countryMap.get(i).getCapital());
-        }
-    return countryOptionsMap;
-  }
+//  public Map<String, String> countryOptions(int difficulty) {
+//    Map<Integer, Country> countryMap = countryService.options(difficulty);
+//    Map<String, String> countryOptionsMap = new HashMap<String, String>();
+//        for (int i = 0; i < countryMap.size(); i++) {
+//          countryOptionsMap.put(countryMap.get(i).getName(), countryMap.get(i).getCapital());
+//        }
+//    return countryOptionsMap;
+//  }
 
 //  public String chosenCountryName (Map<Integer, Country> countryMap) {
 //    return ;
 //  }
+
+  public List<String> getCapitalsFromCountryList(List<Country> randomizedCountryList) {
+    List<String> capitalsList = new ArrayList<>();
+    for (int i = 0; i < randomizedCountryList.size(); i++) {
+      capitalsList.add(randomizedCountryList.get(i).getCapital());
+    }
+    return capitalsList;
+  }
+
 }
