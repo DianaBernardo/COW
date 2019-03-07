f06f762b712c84e3c5e4e1cef9eee65cdabde676
Mark Palotas <ppmark@gmail.com>
2019-03-07 09:48:03 +0100

    initialize new branch

--- src/main/java/cow/fedex/Model/GameLogic.java
+++ src/main/java/cow/fedex/Model/GameLogic.java
@@ -5,78 +5,78 @@ import org.springframework.stereotype.Component;
 @Component
 public class GameLogic {
 
-  private int level;
-  private int totalQuestions;
-  private int askedQuestions;
-  private int correctGuesses;
+    private int level;
+    private int totalQuestions;
+    private int askedQuestions;
+    private int correctGuesses;
 
-  public GameLogic() {
-    this.level = 1;
-    this.askedQuestions = 0;
-    this.correctGuesses = 0;
-  }
-
-  public GameLogic(int level, int totalQuestions) {
-    this.level = level += 1;
-    this.totalQuestions = totalQuestions;
-    this.askedQuestions = 0;
-    this.correctGuesses = 0;
-  }
-
-  public GameLogic(int totalQuestions) {
-    this.level = 1;
-    this.totalQuestions = totalQuestions;
-    this.askedQuestions = 0;
-    this.correctGuesses = 0;
-  }
-
-  public boolean score() {
-    double result = (double) (this.correctGuesses / this.askedQuestions);
-    if (result >= 0.8) {
-      return true;
+    public GameLogic() {
+        this.level = 1;
+        this.askedQuestions = 0;
+        this.correctGuesses = 0;
     }
-    return false;
-  }
 
-  public void increaseLevel(int level, int totalQuestions) {
-    if (level <= 5) {
-      if (this.askedQuestions == this.totalQuestions) {
-        if (score()) {
-          GameLogic gameLogic = new GameLogic(level, totalQuestions);
+    public GameLogic(int level, int totalQuestions) {
+        this.level = level += 1;
+        this.totalQuestions = totalQuestions;
+        this.askedQuestions = 0;
+        this.correctGuesses = 0;
+    }
+
+    public GameLogic(int totalQuestions) {
+        this.level = 1;
+        this.totalQuestions = totalQuestions;
+        this.askedQuestions = 0;
+        this.correctGuesses = 0;
+    }
+
+    public boolean score() {
+        double result = (double) (this.correctGuesses / this.askedQuestions);
+        if (result >= 0.8) {
+            return true;
         }
-      }
+        return false;
     }
-  }
 
-  public int getLevel() {
-    return level;
-  }
+    public void increaseLevel(int level, int totalQuestions) {
+        if (level <= 5) {
+            if (this.askedQuestions == this.totalQuestions) {
+                if (score()) {
+                    GameLogic gameLogic = new GameLogic(level, totalQuestions);
+                }
+            }
+        }
+    }
 
-  public void setLevel(int level) {
-    this.level = level;
-  }
+    public int getLevel() {
+        return level;
+    }
 
-  public int getTotalQuestions() {
-    return totalQuestions;
-  }
+    public void setLevel(int level) {
+        this.level = level;
+    }
 
-  public void setTotalQuestions(int totalQuestions) {
-    this.totalQuestions = totalQuestions;
-  }
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
 
-  public int getAskedQuestions() {
-    return askedQuestions;
-  }
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
 
-  public void setAskedQuestions(int askedQuestions) {
-    this.askedQuestions = askedQuestions;
-  }
+    public int getAskedQuestions() {
+        return askedQuestions;
+    }
 
-  public int getCorrectGuesses() {
-    return correctGuesses;
-  }
+    public void setAskedQuestions(int askedQuestions) {
+        this.askedQuestions = askedQuestions;
+    }
 
-  public void setCorrectGuesses(int correctGuesses) {
-    this.correctGuesses = correctGuesses;
-  }
+    public int getCorrectGuesses() {
+        return correctGuesses;
+    }
+
+    public void setCorrectGuesses(int correctGuesses) {
+        this.correctGuesses = correctGuesses;
+    }
 }
